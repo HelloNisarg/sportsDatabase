b34b7925e766f33bce3b33db5aa3bf29f0e942c0
Nisarg Vankawala <110610685+HelloNisarg@users.noreply.github.com>
2023-04-15 11:36:57 +0530

    Delete urls.py

--- urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.recent, name='recent'),
-    path('team/', views.index, name='index'),
-    path('stats/', views.stats, name='stats'),
-    path('match/', views.matches, name='match')
-]
