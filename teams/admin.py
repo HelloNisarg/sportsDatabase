7390b39685a5cd6fc3cfedf0a11710c409f2422d
Nisarg Vankawala <110610685+HelloNisarg@users.noreply.github.com>
2023-04-17 12:44:03 +0530

    Update admin.py

--- teams/admin.py
+++ teams/admin.py
@@ -2,8 +2,7 @@ from django.contrib import admin
 from .models import ScoreField,Team,Owner,Coach,Match,Player,Captain,Match_Stats
 
 # Register your models here.
-#passwords:hustlers@123
-#user : sports
+
 admin.site.register(Team)
 admin.site.register(Match)
 admin.site.register(Player)
